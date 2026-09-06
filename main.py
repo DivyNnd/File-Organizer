c635945aad0f1518d502181ecdd18aee0a55dbda
Divya Nand <tripathi.divyanand@gmail.com>
2026-09-07 03:42:27 +0530

    File organizer through extensions v1

---
+++ main.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import shutil
+
+root = Path("C:/Users/dntri/Downloads")
+extension_map = {
+    ".jpg": "Images",
+    ".pdf": "Documents",
+    ".exe": "Applications",
+}
+count={}
+
+for subdir in root.iterdir():
+    if subdir.is_file():
+        ext=subdir.suffix.lower()
+        dest=root / extension_map.get(ext,"Other")
+        try:
+            dest.mkdir(parents=True,exist_ok=True)
+            shutil.copy(str(subdir),str(dest/subdir.name))
+            count[dest]=count.get(dest,0)+1
+        except Exception as e:
+            print(f"Error moving {subdir.name}: {e}")
+
+for category,num in count.items():
+    print(f"Number of files copied in {category.name} is {num}.")
