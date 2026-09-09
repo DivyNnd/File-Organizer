ed031192c1de5ab5404ce755fbf0e73a39ebb4af
Divya Nand <tripathi.divyanand@gmail.com>
2026-09-10 03:15:13 +0530

    Updated the code to handle naming conflicts

--- maincli.py
+++ maincli.py
@@ -19,12 +19,21 @@ for subdir in root.iterdir():
     if subdir.is_file():
         ext=subdir.suffix.lower()
         dest=root / extension_map.get(ext,"Other")
+        dest_file=dest/subdir.name
+        if dest_file.exists():
+            seq=1
+            new_name=f"{dest_file.stem}+{seq:02d}{dest_file.suffix}"
+            while (dest/new_name).exists():
+                seq+=1
+                new_name=f"{dest_file.stem}_{seq:02d}{dest_file.suffix}"
+            dest_file=dest/new_name
+            print(f"{subdir.name} changed to {new_name}")
         try:
             if args.dry_run:
                 print(f"{subdir.name} will be moved to {dest.name}")
             else:
                 dest.mkdir(parents=True,exist_ok=True)
-                shutil.move(str(subdir),str(dest/subdir.name))
+                shutil.move(str(subdir),str(dest_file))
                 count[dest]=count.get(dest,0)+1
         except Exception as e:
             print(f"Error moving {subdir.name}: {e}")
